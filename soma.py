237869e3b398b883d9832745aeb81780f2cc45db
Giuzucca <107043130+Giuzucca@users.noreply.github.com>
2025-03-14 22:30:15 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,15 +1,19 @@
 import argparse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-  parser.add_argument("-a", "--a",type=str, required=True)
-  parser.add_argument("-b", "--b",type=str, required=True)
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s --a a --b b",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-a", "--a", type=str, required=True)
+    parser.add_argument("-b", "--b", type=str, required=True)
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  a=args.a
-  b=args.b
+    a = args.a
+    b = args.b
 
-  soma-a+b
+    soma = int(a) + int(b)
 
-  print(soma)
+    print(soma)
